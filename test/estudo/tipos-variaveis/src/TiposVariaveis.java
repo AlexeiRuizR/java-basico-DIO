b9863c7708fdd4f8bfd78b7710c452e3c7256fda
Alexei Ruiz <alexeiruizrodri@gmail.com>
2023-01-12 10:44:02 -0300

    Leccion de variaveis

--- test/estudo/tipos-variaveis/src/TiposVariaveis.java
+++ test/estudo/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinino = 2500.33;
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;  
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        final double PI_EXATA = 3.14;
+        System.out.println(numero + " " + PI_EXATA);
     }
 }
